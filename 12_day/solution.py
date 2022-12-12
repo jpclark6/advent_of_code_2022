8558a12a5c1886c4de01c8889a108c720b168f50
Justin Clark <justin.clark@planet.com>
2022-12-12 15:34:21 -0700

    solves day 12

--- 12_day/solution.py
+++ 12_day/solution.py
@@ -1,4 +1,5 @@
 import os
+from collections import namedtuple
 
 
 filename = "example.txt"
@@ -9,123 +10,95 @@ filepath = directory + "/" + filename
 with open(filepath, "r") as f:
     rows = f.read().splitlines()
 
-
 TOPO = [list(x) for x in rows]
-DIRS = ((0, 1), (0, -1), (1, 0), (-1, 0))
+Dir = namedtuple('Dir', ['x', 'y'])
+DIRS = [Dir(0, 1), Dir(0, -1), Dir(1, 0), Dir(-1, 0)]
+Position = namedtuple('Position', ['x', 'y'])
 
 
-def find_start(start="S"):
-    points = []
-    for y in range(len(TOPO)):
-        for x in range(len(TOPO[y])):
-            if TOPO[y][x] == start:
-                points.append((x, y))
-    return points
+class Path:
+    def __init__(self, start="S"):
+        self.next_positions = self.find_start_positions(start)
+        self.positions = []
+        self.steps = 0
+        self.visited = set()
+        for position in self.next_positions:
+            self.visit(position)
 
+    def find_start_positions(self, start):
+        positions = []
+        for y in range(len(TOPO)):
+            for x in range(len(TOPO[y])):
+                if TOPO[y][x] == start:
+                    positions.append(Position(x, y))
+        return positions
 
-(x, y) = find_start()[0]
-TOPO[y][x] = "a"
+    def is_end(self, position):
+        return TOPO[position.y][position.x] == "E"
 
+    def visit(self, position):
+        self.visited.add((position.x, position.y))
 
-def find_next_moves(history):
-    x = history[-1]["x"]
-    y = history[-1]["y"]
-    steps = history[-1]["steps"]
-    steps += 1
-    next_moves = []
-    for dir in DIRS:
-        next_x = x + dir[0]
-        next_y = y + dir[1]
-        if (
-            next_x >= 0  # must be in the playing grid
-            and next_y >= 0
-            and next_x < len(TOPO[0])
-            and next_y < len(TOPO)
-            and (  # next step can be at max 1 above, unlimited below, but not the finish unless we're at "z"
-                ord(TOPO[next_y][next_x]) - ord(TOPO[y][x]) <= 1
-                and TOPO[next_y][next_x] != "E"
-                or (TOPO[y][x] == "z" and TOPO[next_y][next_x] == "E")
-            )
-        ):
-            new_history = history + [{"x": next_x, "y": next_y, "steps": steps}]
-            next_moves.append(
-                {"x": next_x, "y": next_y, "steps": steps, "history": new_history}
-            )
-    return next_moves
+    def find_end(self):
+        while True:
+            self.take_step()
+            for position in self.positions:
+                for next_position in self.find_next_positions(position):
+                    if self.is_end(next_position):
+                        return self.steps
+                    else:
+                        self.next_positions.append(next_position)
 
+    def take_step(self):
+        self.positions = self.next_positions
+        self.next_positions = []
+        self.steps += 1
 
-visited = {(x, y)}
-
-
-def solve_part_1():
-    next_moves = [
-        {"x": x, "y": y, "steps": 0, "history": [{"x": x, "y": y, "steps": 0}]}
-    ]
-    temp_next_moves = []
-    while True:
-        try:
-            next_move = next_moves.pop()
-            for possible_move in find_next_moves(next_move["history"]):
-                if (possible_move["x"], possible_move["y"]) in visited:
-                    # we've already visited this spot in less moves
-                    continue
-                elif (
-                    TOPO[next_move["y"]][next_move["x"]] == "z"
-                    and TOPO[possible_move["y"]][possible_move["x"]] == "E"
-                ):
-                    # found the end
-                    return possible_move
-                elif TOPO[next_move["y"]][next_move["x"]] != "E":
-                    # continue onwards
-                    temp_next_moves.append(possible_move)
-                    visited.add((possible_move["x"], possible_move["y"]))
-        except IndexError:
-            next_moves = temp_next_moves
-            temp_next_moves = []
-
-
-ans = solve_part_1()
-print("Part 1:", ans["steps"])
-
-
-### PART 2 ###
-
-visited = set(find_start("a"))
-
-
-def solve_part_2():
-    next_moves = []
-    for visit in visited:
-        next_moves.append(
-            {
-                "x": visit[0],
-                "y": visit[1],
-                "steps": 0,
-                "history": [{"x": visit[0], "y": visit[1], "steps": 0}],
-            }
+    def in_topo(self, position):
+        return (
+            position.x >= 0
+            and position.y >= 0
+            and position.x < len(TOPO[0])
+            and position.y < len(TOPO)
         )
-    temp_next_moves = []
-    while True:
-        try:
-            next_move = next_moves.pop()
-            for possible_move in find_next_moves(next_move["history"]):
-                if (possible_move["x"], possible_move["y"]) in visited:
-                    # we've already visited this spot in less moves
-                    continue
-                elif (
-                    TOPO[next_move["y"]][next_move["x"]] == "z"
-                    and TOPO[possible_move["y"]][possible_move["x"]] == "E"
-                ):
-                    # found the end
-                    return possible_move
-                elif TOPO[next_move["y"]][next_move["x"]] != "E":
-                    # continue onwards
-                    temp_next_moves.append(possible_move)
-                    visited.add((possible_move["x"], possible_move["y"]))
-        except IndexError:
-            next_moves = temp_next_moves
-            temp_next_moves = []
+
+    def possible_position(self, p1, p2):
+        # if we've visited it already then ignore it
+        if (p2.x, p2.y) in self.visited:
+            return False
+
+        # must be within 1 if going up, unlimited if going down unless it's
+        # the end, in which case we must be on 'z'
+        in_topo = self.in_topo(p2)
+        if not in_topo:
+            return False
+
+        elevation_valid = (
+            ord(TOPO[p2.y][p2.x]) - ord(TOPO[p1.y][p1.x]) <= 1
+            or TOPO[p1.y][p1.x] == 'S' and TOPO[p2.y][p2.x] == 'a'
+        )
+        is_ending = TOPO[p2.y][p2.x] == "E"
+        ready_for_ending = TOPO[p1.y][p1.x] == "z"
+        return (
+            elevation_valid and not is_ending
+            or is_ending and ready_for_ending
+        )
+
+    def find_next_positions(self, position):
+        next_positions222 = []
+        for dir in DIRS:
+            next_position = Position(
+                position.x + dir.x,
+                position.y + dir.y,
+            )
+            if self.possible_position(position, next_position):
+                next_positions222.append(next_position)
+                self.visit(next_position)
+        return next_positions222
 
 
-ans = solve_part_2()
-print("Part 2:", ans["steps"])
+path_1 = Path(start='S')
+print("Part 1:", path_1.find_end())
+
+path_2 = Path(start='a')
+print("Part 2:", path_2.find_end())
