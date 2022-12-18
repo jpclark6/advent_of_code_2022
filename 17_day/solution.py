4f8a72e50a78d8b518340cdc53c2b7c1710d48c6
Justin Clark <jus@Justins-MacBook-Air.local>
2022-12-17 21:05:17 -0700

    refactor day 17 from minutes to 1/3 second

--- 17_day/solution.py
+++ 17_day/solution.py
@@ -18,7 +18,6 @@ rock_1 = [(0, 0), (1, 0), (2, 0), (3, 0)]
 # .#.
 rock_2 = [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)]
 
-
 # ..#
 # ..#
 # ###
@@ -45,10 +44,11 @@ class Chamber:
     def __init__(self, rocks, wind):
         self.rocks = cycle(rocks)
         self.wind = cycle(list(wind))
-        self.chamber = []
+        self.chamber = {}
         for w in range(self.WIDTH):
-            self.chamber.append((w, 0))
+            self.chamber[(w, 0)] = None
         self.cycles = 0
+        self.top = 0
 
     def has_overlap(self, rock):
         for spot in rock:
@@ -58,7 +58,7 @@ class Chamber:
 
     def push_wind(self, rock):
         dir = next(self.wind)
-        push = 1 if dir == '>' else -1
+        push = 1 if dir == ">" else -1
         new_rock = []
         for spot in rock:
             new_rock.append((spot[0] + push, spot[1]))
@@ -100,121 +100,121 @@ class Chamber:
 
     def add_to_chamber(self, rock):
         for spot in rock:
-            self.chamber.append(spot)
+            self.chamber[spot] = None
+            if spot[1] > self.top:
+                self.top = spot[1]
 
     def find_starting_loc(self):
-        # Each rock appears so that its left edge is two
-        # units away from the left wall and its bottom edge
-        # is three units above the highest rock in the room
-        # (or the floor, if there isn't one).
-        top_rock = max(self.chamber, key=lambda x: x[1], default=0)
-        starting_y = top_rock[1] + self.Y_OFFSET
+        starting_y = self.top + self.Y_OFFSET
         starting_x = self.X_OFFSET
         rock_coords = next(self.rocks)
         rock = []
         for val in rock_coords:
-            rock.append((
-                val[0] + starting_x,
-                val[1] + starting_y,
-            ))
+            rock.append(
+                (
+                    val[0] + starting_x,
+                    val[1] + starting_y,
+                )
+            )
         return rock
 
 
 chamber = Chamber(rocks, wind)
 
+
 def print_chamber(chamber):
+    chamber = chamber.keys()
     printer = []
     for y in range(30):
-        row = '|'
+        row = "|"
         if y == 0:
-            row += 7*'-'
+            row += 7 * "-"
         else:
             for x in range(7):
-                next_char = ''
+                next_char = ""
                 for rock in chamber:
                     if rock[0] == x and rock[1] == y:
-                        next_char = '#'
+                        next_char = "#"
                         break
                 if not next_char:
-                    next_char = '.'
+                    next_char = "."
                 row += next_char
-        row += '|'
-        printer.insert(0, ''.join(row))
+        row += "|"
+        printer.insert(0, "".join(row))
     for row in printer:
         print(row)
 
 
-# for i in range(2022):
-#     chamber.run_rock_drop()
-#     # print_chamber(chamber.chamber)
+## Part 1 ##
+for i in range(2022):
+    chamber.run_rock_drop()
 
-# print("Part 1:", max(chamber.chamber, key=lambda x: x[1])[1])
+print("Part 1:", chamber.top)
 
-GUESS = 11000
 
+## Part 2 ##
+chamber = Chamber(rocks, wind)
+GUESS = 10000
+
+
+"""
+Make an array of heights so we can find if the heights
+repeat at some point, which will let us extrapolate
+"""
 heights = []
 for i in range(GUESS):
-    if i % 100 == 0:
-        print("1st part:", i, "/", GUESS)
     chamber.run_rock_drop()
-    # print_chamber(chamber.chamber)
-    heights.append(max(chamber.chamber, key=lambda x: x[1])[1])
+    heights.append(chamber.top)
 
-# heights = max height after each rock is dropped
-
-diffs = []
-
-hashes = []
-start_round = 100
-min_thing = None
-max_thing = 0
-for i in range(start_round, GUESS):
-    if i % 100 == 0:
-        print("2nd part:", i, "/", GUESS)
-    to_hash_pre = heights[i - start_round:i]
-    to_hash = tuple([x - to_hash_pre[0] for x in to_hash_pre])
-    if to_hash in hashes:
-        if min_thing is None:
-            min_thing = hashes.index(to_hash)
-            max_thing = min_thing
+"""
+Find the repeat pattern. Take a slice of heights normalized
+to the smallest height, find the hash, and see if you start
+to see the same thing over and over. Find an initial offset
+and how many rocks are in the sequence
+"""
+height_pattern_hashes = []
+sequence_length = 100
+unique_order = None
+end_of_sequence = 0
+for i in range(sequence_length, GUESS):
+    sequence = heights[i - sequence_length : i]
+    normalized_sequence = tuple([x - sequence[0] for x in sequence])
+    if normalized_sequence in height_pattern_hashes:
+        if unique_order is None:
+            unique_order = height_pattern_hashes.index(normalized_sequence)
+            end_of_sequence = unique_order
         else:
-            if hashes.index(to_hash) >= max_thing:
-                max_thing = hashes.index(to_hash)
-                starter = i
+            if height_pattern_hashes.index(normalized_sequence) >= end_of_sequence:
+                end_of_sequence = height_pattern_hashes.index(normalized_sequence)
+                start_check_here = i
             else:
                 break
-    hashes.append(to_hash)
+    height_pattern_hashes.append(normalized_sequence)
 
+rocks_in_cycle = end_of_sequence - unique_order + 1
+initial_offset = unique_order
 
-def equalizeit(a):
-    print([x - a[0] for x in a])
+"""
+Do math to calculate height. It's really that final 'else'
+that is all that is needed to calculate big numbers, but
+this lets you calculate any height based on the number of rocks
+"""
+rock_height = 1000000000000
+rock_height -= 1  # To correct for off by 1. Because...?
 
-rocks = max_thing - min_thing + 1
-offset = min_thing
-
-rock = 1000000000000 - 1
-
-if rock < offset + 5:
-    height = heights[rock]
+if rock_height < initial_offset + 5:
+    height = heights[rock_height]
 else:
-    if rock < offset + rocks:
+    if rock_height < initial_offset + rocks_in_cycle:
         height = 0
-        height += heights[offset]
-        height += heights[rock] - heights[offset]
+        height += heights[initial_offset]
+        height += heights[rock_height] - heights[initial_offset]
     else:
-        multi = heights[starter + rocks] - heights[starter]
+        multi = heights[start_check_here + rocks_in_cycle] - heights[start_check_here]
         height = 0
-        height += heights[offset]
-        height += multi * ((rock - offset) // rocks)
-        remainder = (rock - offset) % rocks
-        height += heights[offset + remainder] - heights[offset]
-        # if heights[rock] != height:
-        #     import pdb; pdb.set_trace()
-    # more = (rock - offset) // rocks % rocks
-    # height += heights[offset + more]
-    # height += (rock - offset) // rocks * rocks
+        height += heights[initial_offset]
+        height += multi * ((rock_height - initial_offset) // rocks_in_cycle)
+        remainder = (rock_height - initial_offset) % rocks_in_cycle
+        height += heights[initial_offset + remainder] - heights[initial_offset]
+
 print("Part 2:", height)
-
-# 1514285714288
-# 1514285714288
-
