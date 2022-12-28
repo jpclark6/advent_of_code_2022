033fea17ad7d7f66039317773c9c357f6125b223
Justin Clark <jus@Justins-MacBook-Air.local>
2022-12-28 12:37:15 -0700

    solves day 16

--- 16_day/solution.py
+++ 16_day/solution.py
@@ -40,7 +40,7 @@ class Node:
     def __init__(self, id, flow_rate, connections):
         self.id = id
         self.flow_rate = int(flow_rate)
-        self.connections = connections.split(', ')
+        self.connections = connections.split(", ")
 
     def __repr__(self) -> str:
         return f'< {self.id} - {self.flow_rate} - {",".join([x.id for x in self.connections])}'
@@ -50,44 +50,42 @@ non_zero_flows = dict()
 nodes = set()
 distances = defaultdict(lambda: 99999)
 
-p = re.compile('Valve (.*) has flow rate=(.*); tunnel(?:s?) lead(?:s?) to valve(?:s?) (.*)')
+p = re.compile(
+    "Valve (.*) has flow rate=(.*); tunnel(?:s?) lead(?:s?) to valve(?:s?) (.*)"
+)
 for line in input:
     id, flow_rate, connections = p.findall(line)[0]
-    if flow_rate != '0':
+    if flow_rate != "0":
         non_zero_flows[id] = int(flow_rate)
     nodes.add(id)
-    for conn in connections.split(', '):
+    for conn in connections.split(", "):
         distances[id, conn] = 1
+non_zero_nodes = set(non_zero_flows)
 
 for k, i, j in product(nodes, repeat=3):  # floyd-warshall
-    distances[i,j] = min(distances[i,j], distances[i,k] + distances[k,j])
+    distances[i, j] = min(distances[i, j], distances[i, k] + distances[k, j])
+
 
 @cache
-def search(time, non_zero_flows_set=frozenset(non_zero_flows), current_node='AA'):
-    answers = []
-    for id in non_zero_flows_set:
-        if distances[current_node, id] > time:
+def search(time, non_zero_nodes, location="AA", split_for_elephant=False):
+    totals = []
+    for node in non_zero_nodes:
+        distance = distances[location, node]
+        if time < distance:
             continue
-        ans = (
-            non_zero_flows[id] * (time - distances[current_node, id] - 1)
-            + search(time-distances[current_node, id] - 1, non_zero_flows_set - {id}, id))
-        answers.append(ans)
-    return max(answers, default=0)
+        total = (time - distance - 1) * non_zero_flows[node]
+        next_locations = non_zero_nodes - {node}
+        total += search(
+            (time - distance - 1),
+            frozenset(next_locations),
+            location=node,
+            split_for_elephant=split_for_elephant,
+        )
+        totals.append(total)
+    if split_for_elephant:
+        totals.append(search(26, non_zero_nodes))
+    return max(totals, default=0)
 
-print(search(30))
 
-@cache
-def search_p2(time, non_zero_flows_set=frozenset(non_zero_flows), current_node='AA', top_level=False):
-    answers = []
-    for id in non_zero_flows_set:
-        if distances[current_node, id] > time:
-            continue
-        ans = (
-            non_zero_flows[id] * (time - distances[current_node, id] - 1)
-            + search_p2(time-distances[current_node, id] - 1, non_zero_flows_set - {id}, id, top_level=True))
-        answers.append(ans)
-    if top_level:
-        answers.append(search(26, non_zero_flows_set=non_zero_flows_set))
-    return max(answers, default=0)
-
-print(search_p2(26, top_level=True))
+print("Part 1:", search(30, frozenset(non_zero_nodes), "AA"))
+print("Part 2:", search(26, frozenset(non_zero_nodes), "AA", split_for_elephant=True))
